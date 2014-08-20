ed48fde586a02b95c41540232b3e9a63d56d8453
Michael Ford <mtford@gmail.com>
2014-08-20 11:39:26 +0100

    failing imp of foreign key relationship

--- src/relationship.js
+++ src/relationship.js
@@ -1,30 +1,54 @@
 angular.module('restkit.relationship', ['restkit', 'restkit.store'])
 
     .factory('Relationship', function () {
-        function Relationship() {
-
+        function Relationship(name, reverseName, mapping, reverseMapping) {
+            if (!this) {
+                return new Relationship(name, reverseName, mapping);
+            }
+            this.mapping = mapping;
+            this.name = name;
+            this.reverseName = reverseName;
+            this.reverseMapping = reverseMapping;
         }
+        Relationship.prototype.getRelated = function (obj, callback) {
+            throw Error('Relationship.getRelated must be overridden');
+        };
         return Relationship;
     })
 
-    .factory('ManyToManyRelationship', function () {
-        function ManyToManyRelationship() {
-
+    .factory('ManyToManyRelationship', function (Relationship) {
+        function ManyToManyRelationship(name, reverseName, mapping, reverseMapping) {
+            if (!this) {
+                return new ManyToManyRelationship(name, reverseName, mapping, reverseMapping);
+            }
+            Relationship.call(this, mapping);
         }
+        ManyToManyRelationship.prototype = Object.create(Relationship.prototype);
         return ManyToManyRelationship;
     })
 
-    .factory('OneToManyRelationship', function () {
-        function OneToManyRelationship() {
-
+    .factory('ForeignKeyRelationship', function (Relationship) {
+        function ForeignKeyRelationship(name, reverseName, mapping, reverseMapping) {
+            if (!this) {
+                return new ForeignKeyRelationship(name, reverseName, mapping, reverseMapping);
+            }
+            Relationship.call(this, mapping);
         }
-        return OneToManyRelationship;
+        ForeignKeyRelationship.prototype = Object.create(Relationship.prototype);
+        ForeignKeyRelationship.prototype.getRelated = function (obj, callback) {
+
+        };
+        return ForeignKeyRelationship;
     })
 
-    .factory('OneToOneRelationship', function () {
-        function OneToOneRelationship() {
-
+    .factory('OneToOneRelationship', function (Relationship) {
+        function OneToOneRelationship(name, reverseName, mapping, reverseMapping) {
+            if (!this) {
+                return new OneToOneRelationship(name, reverseName, mapping, reverseMapping);
+            }
+            Relationship.call(this, mapping);
         }
+        OneToOneRelationship.prototype = Object.create(Relationship.prototype);
         return OneToOneRelationship;
     })
 
--- src/relationship.spec.js
+++ src/relationship.spec.js
@@ -1,22 +1,55 @@
 describe.only('relationship', function () {
 
-    var RestAPI;
+    var RestAPI, Mapping, ForeignKeyRelationship, RestObject;
 
     beforeEach(function () {
         module('restkit.relationship', function ($provide) {
             $provide.value('$log', console);
             $provide.value('$q', Q);
         });
+        module('restkit.mapping', function ($provide) {
+            $provide.value('$log', console);
+            $provide.value('$q', Q);
+        });
 
-        inject(function (_RestAPI_) {
+        inject(function (_RestAPI_, _Mapping_, _ForeignKeyRelationship_, _RestObject_) {
             RestAPI = _RestAPI_;
+            Mapping = _Mapping_;
+            ForeignKeyRelationship = _ForeignKeyRelationship_;
+            RestObject = _RestObject_;
         });
 
         RestAPI._reset();
     });
 
-    it('dsfsdfsdf', function () {
+    describe('OneToMany', function () {
+        var carMapping, personMapping;
+        beforeEach(function (done) {
+            carMapping = new Mapping({
+                type: 'Car',
+                id: 'id',
+                attributes: ['colour', 'name'],
+                api: 'myApi'
+            });
+            personMapping = new Mapping({
+                type: 'Person',
+                id: 'id',
+                attributes: ['name', 'age'],
+                api: 'myApi'
+            });
+            carMapping.install(function (err) {
+                if (err) done(err);
+                personMapping.install(done);
+            });
+        });
 
+        it('sdas', function (done) {
+            var r = new ForeignKeyRelationship('owner', 'cars', carMapping, personMapping);
+            var car = new RestObject(carMapping);
+            car.ownerId = 5;
+            r.getRelated(car, function (err, related) {
+                done(err);
+            });
+        });
     });
-
 });
