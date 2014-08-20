9cab4a51525e2a58cdc1e1bac323f51c168d6c63
Michael Ford <mtford@gmail.com>
2014-08-20 21:03:25 +0100

    methods

--- src/requestDescriptor.js
+++ src/requestDescriptor.js
@@ -4,15 +4,37 @@ angular.module('restkit.requestDescriptor', ['restkit'])
         // The XRegExp object has these properties that we want to ignore when matching.
         var ignore = ['index', 'input'];
 
+
         function RequestDescriptor(opts) {
+            if (!this) {
+                return new RequestDescriptor(opts);
+            }
+
             this._opts = opts;
+
+            // Convert path string into XRegExp if not already.
             if (this._opts.path) {
                 if (!(this._opts.path instanceof XRegExp)) {
                     this._opts.path = XRegExp(this._opts.path);
                 }
             }
+
+            // Convert wildcards into methods and ensure is an array.
+            if (this._opts.method) {
+                if (this._opts.method == '*' || this._opts.method.indexOf('*') > -1) {
+                    this._opts.method = this.httpMethods;
+                }
+                else if (typeof(this._opts.method) == 'string') {
+                    this._opts.method = [this._opts.method];
+                }
+            }
+
             defineSubProperty.call(this, 'path', this._opts);
+            defineSubProperty.call(this, 'method', this._opts);
         }
+
+        RequestDescriptor.prototype.httpMethods = ['POST', 'PATCH', 'PUT', 'HEAD', 'GET', 'DELETE', 'OPTIONS', 'TRACE', 'CONNECT'];
+
 
         RequestDescriptor.prototype._matchPath = function (path) {
             var match = XRegExp.exec(path, this.path);
--- src/requestDescriptor.spec.js
+++ src/requestDescriptor.spec.js
@@ -16,10 +16,36 @@ describe.only('request descriptor', function () {
         RestAPI._reset();
     });
 
-    it('dsfsdfsdf', function () {
-        var r = new RequestDescriptor({path: '/cars/(?<id>[0-9])/'});
-        console.log(r._matchPath('/cars/5/'));
+    it('match path', function () {
+        var r = new RequestDescriptor({path: '/cars/(?<id>[0-9])/?'});
+        var match = r._matchPath('/cars/5/');
+        assert.equal(match.id, '5');
+        match = r._matchPath('/cars/5');
+        assert.equal(match.id, '5');
+    });
 
+    it('all http methods', function () {
+        var r = new RequestDescriptor({method: '*'});
+        _.each(r.httpMethods, function (method) {
+            assert.include(r.method, method);
+        });
+        r = new RequestDescriptor({method: ['*']});
+        _.each(r.httpMethods, function (method) {
+            assert.include(r.method, method);
+        });
+        r = new RequestDescriptor({method: ['*', 'GET']});
+        _.each(r.httpMethods, function (method) {
+            assert.include(r.method, method);
+        });
+    });
+
+    it('mapping', function () {
+
+        assert.ok(false);
+    });
+
+    it('data', function () {
+        assert.ok(false);
     });
 
 });
