baafc76ac408ebc33dac0f1c5e9b91a72bb49246
Michael Ford <mtford@gmail.com>
2014-09-15 10:05:15 +0100

    extra behaviour tests for raw queries

--- test/rawquery.behaviour.spec.js
+++ test/rawquery.behaviour.spec.js
@@ -16,169 +16,323 @@ describe('raw query behaviour', function () {
 
     describe('data types', function () {
 
-        describe('singular', function () {
+        describe('indexed', function () {
+            describe('singular', function () {
 
-            beforeEach(function (done) {
-                var index = new Index('myCollection', 'Car', ['id']);
-                index.install(done);
-            });
-
-            it('string', function (done) {
-                Pouch.getPouch().post({
-                    type: 'Car',
-                    colour: 'red',
-                    name: 'Aston Martin',
-                    collection: 'myCollection',
-                    id: 'blah'
-                }, function (err, resp) {
-                    if (err) {
-                        done(err);
-                    }
-                    else {
-                        var q = new RawQuery('myCollection', 'Car', {id: 'blah'});
-                        q.execute(function (err, docs) {
-                            if (err) done(err);
-                            assert.equal(docs.length, 1);
-                            done();
-                        });
-                    }
+                beforeEach(function (done) {
+                    var index = new Index('myCollection', 'Car', ['id']);
+                    index.install(done);
                 });
-            });
 
-            it('numeric', function (done) {
-                Pouch.getPouch().post({
-                    type: 'Car',
-                    colour: 'red',
-                    name: 'Aston Martin',
-                    collection: 'myCollection',
-                    id: 5
-                }, function (err, resp) {
-                    if (err) {
-                        done(err);
-                    }
-                    else {
-                        var q = new RawQuery('myCollection', 'Car', {id: 5});
-                        q.execute(function (err, docs) {
-                            if (err) done(err);
-                            dump(docs);
-                            assert.equal(docs.length, 1);
-                            done();
-                        });
-                    }
+                it('string', function (done) {
+                    Pouch.getPouch().post({
+                        type: 'Car',
+                        colour: 'red',
+                        name: 'Aston Martin',
+                        collection: 'myCollection',
+                        id: 'blah'
+                    }, function (err, resp) {
+                        if (err) {
+                            done(err);
+                        }
+                        else {
+                            var q = new RawQuery('myCollection', 'Car', {id: 'blah'});
+                            q.execute(function (err, docs) {
+                                if (err) done(err);
+                                assert.equal(docs.length, 1);
+                                done();
+                            });
+                        }
+                    });
                 });
-            });
 
-            it('numeric with string query', function (done) {
-                Pouch.getPouch().post({
-                    type: 'Car',
-                    colour: 'red',
-                    name: 'Aston Martin',
-                    collection: 'myCollection',
-                    id: 5
-                }, function (err, resp) {
-                    if (err) {
-                        done(err);
-                    }
-                    else {
-                        var q = new RawQuery('myCollection', 'Car', {id: '5'});
-                        q.execute(function (err, docs) {
-                            if (err) done(err);
-                            dump(docs);
-                            assert.equal(docs.length, 1);
-                            done();
-                        });
-                    }
+                it('numeric', function (done) {
+                    Pouch.getPouch().post({
+                        type: 'Car',
+                        colour: 'red',
+                        name: 'Aston Martin',
+                        collection: 'myCollection',
+                        id: 5
+                    }, function (err, resp) {
+                        if (err) {
+                            done(err);
+                        }
+                        else {
+                            var q = new RawQuery('myCollection', 'Car', {id: 5});
+                            q.execute(function (err, docs) {
+                                if (err) done(err);
+                                dump(docs);
+                                assert.equal(docs.length, 1);
+                                done();
+                            });
+                        }
+                    });
                 });
-            });
 
-            it('string with numeric query', function (done) {
-                Pouch.getPouch().post({
-                    type: 'Car',
-                    colour: 'red',
-                    name: 'Aston Martin',
-                    collection: 'myCollection',
-                    id: '5'
-                }, function (err, resp) {
-                    if (err) {
-                        done(err);
-                    }
-                    else {
-                        var q = new RawQuery('myCollection', 'Car', {id: 5});
-                        q.execute(function (err, docs) {
-                            if (err) done(err);
-                            dump(docs);
-                            assert.equal(docs.length, 1);
-                            done();
-                        });
-                    }
+                it('numeric with string query', function (done) {
+                    Pouch.getPouch().post({
+                        type: 'Car',
+                        colour: 'red',
+                        name: 'Aston Martin',
+                        collection: 'myCollection',
+                        id: 5
+                    }, function (err, resp) {
+                        if (err) {
+                            done(err);
+                        }
+                        else {
+                            var q = new RawQuery('myCollection', 'Car', {id: '5'});
+                            q.execute(function (err, docs) {
+                                if (err) done(err);
+                                dump(docs);
+                                assert.equal(docs.length, 1);
+                                done();
+                            });
+                        }
+                    });
                 });
+
+                it('string with numeric query', function (done) {
+                    Pouch.getPouch().post({
+                        type: 'Car',
+                        colour: 'red',
+                        name: 'Aston Martin',
+                        collection: 'myCollection',
+                        id: '5'
+                    }, function (err, resp) {
+                        if (err) {
+                            done(err);
+                        }
+                        else {
+                            var q = new RawQuery('myCollection', 'Car', {id: 5});
+                            q.execute(function (err, docs) {
+                                if (err) done(err);
+                                dump(docs);
+                                assert.equal(docs.length, 1);
+                                done();
+                            });
+                        }
+                    });
+                });
+
+
             });
 
+            describe('multiple', function () {
 
+                beforeEach(function (done) {
+                    var index = new Index('myCollection', 'Car', ['id', 'colour', 'age']);
+                    index.install(done);
+                });
+
+                it('numeric', function (done) {
+                    Pouch.getPouch().post({
+                        type: 'Car',
+                        colour: 'red',
+                        name: 'Aston Martin',
+                        collection: 'myCollection',
+                        id: 5,
+                        age: 2
+                    }, function (err, resp) {
+                        if (err) {
+                            done(err);
+                        }
+                        else {
+                            var q = new RawQuery('myCollection', 'Car', {id: 5, colour: 'red', age: 2});
+                            q.execute(function (err, docs) {
+                                if (err) done(err);
+                                dump(docs);
+                                assert.equal(docs.length, 1);
+                                done();
+                            });
+                        }
+                    });
+                });
+
+                it('string', function (done) {
+                    Pouch.getPouch().post({
+                        type: 'Car',
+                        colour: 'red',
+                        name: 'Aston Martin',
+                        collection: 'myCollection',
+                        id: '5',
+                        age: 2
+                    }, function (err) {
+                        if (err) {
+                            done(err);
+                        }
+                        else {
+                            var q = new RawQuery('myCollection', 'Car', {id: '5', colour: 'red', age: 2});
+                            q.execute(function (err, docs) {
+                                if (err) done(err);
+                                dump(docs);
+                                assert.equal(docs.length, 1);
+                                done();
+                            });
+                        }
+                    });
+                });
+
+
+
+
+            });
         });
 
-        describe('multiple', function () {
+        describe('not indexed', function () {
+            describe('singular', function () {
 
-            beforeEach(function (done) {
-                var index = new Index('myCollection', 'Car', ['id', 'colour', 'age']);
-                index.install(done);
-            });
-
-            it('numeric', function (done) {
-                Pouch.getPouch().post({
-                    type: 'Car',
-                    colour: 'red',
-                    name: 'Aston Martin',
-                    collection: 'myCollection',
-                    id: 5,
-                    age: 2
-                }, function (err, resp) {
-                    if (err) {
-                        done(err);
-                    }
-                    else {
-                        var q = new RawQuery('myCollection', 'Car', {id: 5, colour: 'red', age: 2});
-                        q.execute(function (err, docs) {
-                            if (err) done(err);
-                            dump(docs);
-                            assert.equal(docs.length, 1);
-                            done();
-                        });
-                    }
+                it('string', function (done) {
+                    Pouch.getPouch().post({
+                        type: 'Car',
+                        colour: 'red',
+                        name: 'Aston Martin',
+                        collection: 'myCollection',
+                        id: 'blah'
+                    }, function (err, resp) {
+                        if (err) {
+                            done(err);
+                        }
+                        else {
+                            var q = new RawQuery('myCollection', 'Car', {id: 'blah'});
+                            q.execute(function (err, docs) {
+                                if (err) done(err);
+                                assert.equal(docs.length, 1);
+                                done();
+                            });
+                        }
+                    });
                 });
-            });
 
-            it('string', function (done) {
-                Pouch.getPouch().post({
-                    type: 'Car',
-                    colour: 'red',
-                    name: 'Aston Martin',
-                    collection: 'myCollection',
-                    id: '5',
-                    age: 2
-                }, function (err) {
-                    if (err) {
-                        done(err);
-                    }
-                    else {
-                        var q = new RawQuery('myCollection', 'Car', {id: '5', colour: 'red', age: 2});
-                        q.execute(function (err, docs) {
-                            if (err) done(err);
-                            dump(docs);
-                            assert.equal(docs.length, 1);
-                            done();
-                        });
-                    }
+                it('numeric', function (done) {
+                    Pouch.getPouch().post({
+                        type: 'Car',
+                        colour: 'red',
+                        name: 'Aston Martin',
+                        collection: 'myCollection',
+                        id: 5
+                    }, function (err, resp) {
+                        if (err) {
+                            done(err);
+                        }
+                        else {
+                            var q = new RawQuery('myCollection', 'Car', {id: 5});
+                            q.execute(function (err, docs) {
+                                if (err) done(err);
+                                dump(docs);
+                                assert.equal(docs.length, 1);
+                                done();
+                            });
+                        }
+                    });
                 });
+
+                it('numeric with string query', function (done) {
+                    Pouch.getPouch().post({
+                        type: 'Car',
+                        colour: 'red',
+                        name: 'Aston Martin',
+                        collection: 'myCollection',
+                        id: 5
+                    }, function (err, resp) {
+                        if (err) {
+                            done(err);
+                        }
+                        else {
+                            var q = new RawQuery('myCollection', 'Car', {id: '5'});
+                            q.execute(function (err, docs) {
+                                if (err) done(err);
+                                dump(docs);
+                                assert.equal(docs.length, 1);
+                                done();
+                            });
+                        }
+                    });
+                });
+
+                it('string with numeric query', function (done) {
+                    Pouch.getPouch().post({
+                        type: 'Car',
+                        colour: 'red',
+                        name: 'Aston Martin',
+                        collection: 'myCollection',
+                        id: '5'
+                    }, function (err, resp) {
+                        if (err) {
+                            done(err);
+                        }
+                        else {
+                            var q = new RawQuery('myCollection', 'Car', {id: 5});
+                            q.execute(function (err, docs) {
+                                if (err) done(err);
+                                dump(docs);
+                                assert.equal(docs.length, 1);
+                                done();
+                            });
+                        }
+                    });
+                });
+
+
             });
 
+            describe('multiple', function () {
+
+
+                it('numeric', function (done) {
+                    Pouch.getPouch().post({
+                        type: 'Car',
+                        colour: 'red',
+                        name: 'Aston Martin',
+                        collection: 'myCollection',
+                        id: 5,
+                        age: 2
+                    }, function (err, resp) {
+                        if (err) {
+                            done(err);
+                        }
+                        else {
+                            var q = new RawQuery('myCollection', 'Car', {id: 5, colour: 'red', age: 2});
+                            q.execute(function (err, docs) {
+                                if (err) done(err);
+                                dump(docs);
+                                assert.equal(docs.length, 1);
+                                done();
+                            });
+                        }
+                    });
+                });
+
+                it('string', function (done) {
+                    Pouch.getPouch().post({
+                        type: 'Car',
+                        colour: 'red',
+                        name: 'Aston Martin',
+                        collection: 'myCollection',
+                        id: '5',
+                        age: 2
+                    }, function (err) {
+                        if (err) {
+                            done(err);
+                        }
+                        else {
+                            var q = new RawQuery('myCollection', 'Car', {id: '5', colour: 'red', age: 2});
+                            q.execute(function (err, docs) {
+                                if (err) done(err);
+                                dump(docs);
+                                assert.equal(docs.length, 1);
+                                done();
+                            });
+                        }
+                    });
+                });
 
 
 
+
+            });
         });
 
     });
-
-
 
 });
