fb9acc17100b249e9a07f6a73e10e5d2f8c23269
Michael Ford <mtford@gmail.com>
2015-03-02 07:39:24 +0000

    graph at siesta level

--- core/index.js
+++ core/index.js
@@ -1,197 +1,233 @@
-(function () {
-    var util = require('./util'),
-        CollectionRegistry = require('./collectionRegistry').CollectionRegistry,
-        Collection = require('./collection'),
-        cache = require('./cache'),
-        Model = require('./model'),
-        error = require('./error'),
-        events = require('./events'),
-        RelationshipType = require('./RelationshipType'),
-        ReactiveQuery = require('./ReactiveQuery'),
-        ManyToManyProxy = require('./ManyToManyProxy'),
-        OneToOneProxy = require('./OneToOneProxy'),
-        OneToManyProxy = require('./OneToManyProxy'),
-        RelationshipProxy = require('./RelationshipProxy'),
-        modelEvents = require('./modelEvents'),
-        Query = require('./Query'),
-        querySet = require('./QuerySet'),
-        log = require('./log'),
-        _ = util._;
-    util._patchBind();
+(function() {
+  var util = require('./util'),
+      CollectionRegistry = require('./collectionRegistry').CollectionRegistry,
+      Collection = require('./collection'),
+      cache = require('./cache'),
+      Model = require('./model'),
+      error = require('./error'),
+      events = require('./events'),
+      RelationshipType = require('./RelationshipType'),
+      ReactiveQuery = require('./ReactiveQuery'),
+      ManyToManyProxy = require('./ManyToManyProxy'),
+      OneToOneProxy = require('./OneToOneProxy'),
+      OneToManyProxy = require('./OneToManyProxy'),
+      RelationshipProxy = require('./RelationshipProxy'),
+      modelEvents = require('./modelEvents'),
+      Query = require('./Query'),
+      querySet = require('./QuerySet'),
+      log = require('./log'),
+      _ = util._;
+  util._patchBind();
 
-    // Initialise siesta object. Strange format facilities using submodules with requireJS (eventually)
-    var siesta = function (ext) {
-        if (!siesta.ext) siesta.ext = {};
-        _.extend(siesta.ext, ext || {});
-        return siesta;
-    };
+  // Initialise siesta object. Strange format facilities using submodules with requireJS (eventually)
+  var siesta = function(ext) {
+    if (!siesta.ext) siesta.ext = {};
+    _.extend(siesta.ext, ext || {});
+    return siesta;
+  };
 
-    // Notifications
-    _.extend(siesta, {
-        on: events.on.bind(events),
-        off: events.removeListener.bind(events),
-        once: events.once.bind(events),
-        removeAllListeners: events.removeAllListeners.bind(events)
-    });
-    _.extend(siesta, {
-        removeListener: siesta.off,
-        addListener: siesta.on
-    });
+  // Notifications
+  _.extend(siesta, {
+    on: events.on.bind(events),
+    off: events.removeListener.bind(events),
+    once: events.once.bind(events),
+    removeAllListeners: events.removeAllListeners.bind(events)
+  });
+  _.extend(siesta, {
+    removeListener: siesta.off,
+    addListener: siesta.on
+  });
 
-    // Expose some stuff for usage by extensions and/or users
-    _.extend(siesta, {
-        RelationshipType: RelationshipType,
-        ModelEventType: modelEvents.ModelEventType,
-        log: log.Level,
-        InsertionPolicy: ReactiveQuery.InsertionPolicy,
-        _internal: {
-            log: log,
-            Model: Model,
-            error: error,
-            ModelEventType: modelEvents.ModelEventType,
-            ModelInstance: require('./ModelInstance'),
-            extend: require('extend'),
-            MappingOperation: require('./mappingOperation'),
-            events: events,
-            ProxyEventEmitter: events.ProxyEventEmitter,
-            cache: require('./cache'),
-            modelEvents: modelEvents,
-            CollectionRegistry: require('./collectionRegistry').CollectionRegistry,
-            Collection: Collection,
-            utils: util,
-            util: util,
-            _: util._,
-            querySet: querySet,
-            observe: require('../vendor/observe-js/src/observe'),
-            Query: Query,
-            Store: require('./store'),
-            ManyToManyProxy: ManyToManyProxy,
-            OneToManyProxy: OneToManyProxy,
-            OneToOneProxy: OneToOneProxy,
-            RelationshipProxy: RelationshipProxy
-        },
-        _: util._,
-        async: util.async,
-        isArray: util.isArray,
-        isString: util.isString
-    });
+  // Expose some stuff for usage by extensions and/or users
+  _.extend(siesta, {
+    RelationshipType: RelationshipType,
+    ModelEventType: modelEvents.ModelEventType,
+    log: log.Level,
+    InsertionPolicy: ReactiveQuery.InsertionPolicy,
+    _internal: {
+      log: log,
+      Model: Model,
+      error: error,
+      ModelEventType: modelEvents.ModelEventType,
+      ModelInstance: require('./ModelInstance'),
+      extend: require('extend'),
+      MappingOperation: require('./mappingOperation'),
+      events: events,
+      ProxyEventEmitter: events.ProxyEventEmitter,
+      cache: require('./cache'),
+      modelEvents: modelEvents,
+      CollectionRegistry: require('./collectionRegistry').CollectionRegistry,
+      Collection: Collection,
+      utils: util,
+      util: util,
+      _: util._,
+      querySet: querySet,
+      observe: require('../vendor/observe-js/src/observe'),
+      Query: Query,
+      Store: require('./store'),
+      ManyToManyProxy: ManyToManyProxy,
+      OneToManyProxy: OneToManyProxy,
+      OneToOneProxy: OneToOneProxy,
+      RelationshipProxy: RelationshipProxy
+    },
+    _: util._,
+    async: util.async,
+    isArray: util.isArray,
+    isString: util.isString
+  });
 
-    siesta.ext = {};
+  siesta.ext = {};
 
-    var installed = false,
-        installing = false;
+  var installed = false,
+      installing = false;
 
 
-    _.extend(siesta, {
-        /**
-         * Wipe everything. Used during test generally.
-         */
-        reset: function (cb) {
-            installed = false;
-            installing = false;
+  _.extend(siesta, {
+    /**
+     * Wipe everything. Used during test generally.
+     */
+    reset: function(cb) {
+      installed = false;
+      installing = false;
+      delete this.queuedTasks;
+      cache.reset();
+      CollectionRegistry.reset();
+      events.removeAllListeners();
+      if (siesta.ext.storageEnabled) {
+        siesta.ext.storage._reset(cb);
+      }
+      else {
+        cb();
+      }
+    },
+    /**
+     * Creates and registers a new Collection.
+     * @param  {String} name
+     * @param  {Object} [opts]
+     * @return {Collection}
+     */
+    collection: function(name, opts) {
+      return new Collection(name, opts);
+    },
+    /**
+     * Install all collections.
+     * @param {Function} [cb]
+     * @returns {q.Promise}
+     */
+    install: function(cb) {
+      if (!installing && !installed) {
+        return util.promise(cb, function(cb) {
+          installing = true;
+          var collectionNames = CollectionRegistry.collectionNames,
+              tasks = _.map(collectionNames, function(n) {
+                return CollectionRegistry[n].install.bind(CollectionRegistry[n]);
+              }),
+              storageEnabled = siesta.ext.storageEnabled;
+          if (storageEnabled) tasks = tasks.concat([siesta.ext.storage.ensureIndexesForAll, siesta.ext.storage._load]);
+          tasks.push(function(done) {
+            installed = true;
+            if (this.queuedTasks) this.queuedTasks.execute();
+            done();
+          }.bind(this));
+          siesta.async.series(tasks, cb);
+        }.bind(this));
+      }
+      else cb(error('already installing'));
+    },
+    _pushTask: function(task) {
+      if (!this.queuedTasks) {
+        this.queuedTasks = new function Queue() {
+          this.tasks = [];
+          this.execute = function() {
+            this.tasks.forEach(function(f) {
+              f()
+            });
+            this.tasks = [];
+          }.bind(this);
+        };
+      }
+      this.queuedTasks.tasks.push(task);
+    },
+    _afterInstall: function(task) {
+      if (!installed) {
+        if (!installing) {
+          this.install(function(err) {
+            if (err) console.error('Error setting up siesta', err);
             delete this.queuedTasks;
-            cache.reset();
-            CollectionRegistry.reset();
-            events.removeAllListeners();
-            if (siesta.ext.storageEnabled) {
-                siesta.ext.storage._reset(cb);
-            }
-            else {
-                cb();
-            }
-        },
-        /**
-         * Creates and registers a new Collection.
-         * @param  {String} name
-         * @param  {Object} [opts]
-         * @return {Collection}
-         */
-        collection: function (name, opts) {
-            return new Collection(name, opts);
-        },
-        /**
-         * Install all collections.
-         * @param {Function} [cb]
-         * @returns {q.Promise}
-         */
-        install: function (cb) {
-            if (!installing && !installed) {
-                return util.promise(cb, function (cb) {
-                    installing = true;
-                    var collectionNames = CollectionRegistry.collectionNames,
-                        tasks = _.map(collectionNames, function (n) {
-                            return CollectionRegistry[n].install.bind(CollectionRegistry[n]);
-                        }),
-                        storageEnabled = siesta.ext.storageEnabled;
-                    if (storageEnabled) tasks = tasks.concat([siesta.ext.storage.ensureIndexesForAll, siesta.ext.storage._load]);
-                    tasks.push(function (done) {
-                        installed = true;
-                        if (this.queuedTasks) this.queuedTasks.execute();
-                        done();
-                    }.bind(this));
-                    siesta.async.series(tasks, cb);
-                }.bind(this));
-            }
-            else cb(error('already installing'));
-        },
-        _pushTask: function (task) {
-            if (!this.queuedTasks) {
-                this.queuedTasks = new function Queue() {
-                    this.tasks = [];
-                    this.execute = function () {
-                        this.tasks.forEach(function (f) {
-                            f()
-                        });
-                        this.tasks = [];
-                    }.bind(this);
-                };
-            }
-            this.queuedTasks.tasks.push(task);
-        },
-        _afterInstall: function (task) {
-            if (!installed) {
-                if (!installing) {
-                    this.install(function (err) {
-                        if (err) console.error('Error setting up siesta', err);
-                        delete this.queuedTasks;
-                    }.bind(this));
-                }
-                // In case installed straight away e.g. if storage extension not installed.
-                if (!installed) this._pushTask(task);
-                else task();
-            }
-            else {
-                task();
-            }
-        },
-        setLogLevel: function (loggerName, level) {
-            var Logger = log.loggerWithName(loggerName);
-            Logger.setLevel(level);
-        },
-        notify: util.next,
-        registerComparator: Query.registerComparator.bind(Query)
-    });
-
-    Object.defineProperties(siesta, {
-        _canChange: {
-            get: function () {
-                return !(installing || installed);
-            }
+          }.bind(this));
         }
-    });
+        // In case installed straight away e.g. if storage extension not installed.
+        if (!installed) this._pushTask(task);
+        else task();
+      }
+      else {
+        task();
+      }
+    },
+    setLogLevel: function(loggerName, level) {
+      var Logger = log.loggerWithName(loggerName);
+      Logger.setLevel(level);
+    },
+    graph: function(data, opts, cb) {
+      if (typeof opts == 'function') cb = opts;
+      opts = opts || {};
+      return util.promise(cb, function(cb) {
+        var tasks = [], err;
+        for (var collectionName in data) {
+          if (data.hasOwnProperty(collectionName)) {
+            var collection = CollectionRegistry[collectionName];
+            if (collection) {
+              (function(collection, data) {
+                tasks.push(function(done) {
+                  collection.graph(data, function(err, res) {
+                    if (!err) {
+                      var results = {};
+                      results[collection.name] = res;
+                    }
+                    done(err, results);
+                  });
+                });
+              })(collection, data[collectionName]);
+            }
+            else {
+              err = 'No such collection "' + collectionName + '"';
+            }
+          }
+        }
+        if (!err) util.async.series(tasks, function(err, results) {
+          if (!err) {
+            results = results.reduce(function(memo, res) {
+              return _.extend(memo, res);
+            }, {})
+          } else results = null;
+          cb(err, results);
+        });
+        else cb(error(err, {data: data, invalidCollectionName: collectionName}));
+      }.bind(this));
+    },
+    notify: util.next,
+    registerComparator: Query.registerComparator.bind(Query)
+  });
 
-    if (typeof window != 'undefined') {
-        window['siesta'] = siesta;
+  Object.defineProperties(siesta, {
+    _canChange: {
+      get: function() {
+        return !(installing || installed);
+      }
     }
+  });
 
-    siesta.log = require('debug');
+  if (typeof window != 'undefined') {
+    window['siesta'] = siesta;
+  }
 
-    module.exports = siesta;
+  siesta.log = require('debug');
+
+  module.exports = siesta;
 
 
-
-    (function loadExtensions() {
-        require('../storage');
-    })();
+  (function loadExtensions() {
+    require('../storage');
+  })();
 
 })();
--- test/highlevel.spec.js
+++ test/highlevel.spec.js
@@ -4,24 +4,25 @@ var assert = require('chai').assert,
     RelationshipType = siesta.RelationshipType;
 
 describe('higher level mapping', function() {
-  var Collection, PersonModel, CarModel;
 
-  beforeEach(function(done) {
-    siesta.reset(function() {
-      Collection = siesta.collection('myCollection');
-      PersonModel = Collection.model('Person', {
-        id: 'id',
-        attributes: ['name', 'age']
-      });
-      CarModel = Collection.model('Car', {
-        id: 'id',
-        attributes: ['colour']
-      });
-      done();
-    });
-  });
 
   describe('collection level', function() {
+    var Collection, PersonModel, CarModel;
+
+    beforeEach(function(done) {
+      siesta.reset(function() {
+        Collection = siesta.collection('myCollection');
+        PersonModel = Collection.model('Person', {
+          id: 'id',
+          attributes: ['name', 'age']
+        });
+        CarModel = Collection.model('Car', {
+          id: 'id',
+          attributes: ['colour']
+        });
+        done();
+      });
+    });
     it('map singles', function(done) {
       Collection.graph({
         Car: {colour: 'red', id: 5}
@@ -68,4 +69,118 @@ describe('higher level mapping', function() {
       });
     });
   });
+
+  describe('siesta level', function() {
+    var Collection, PersonModel, CarModel, OtherCollection, OtherModel;
+
+
+    beforeEach(function(done) {
+      siesta.reset(function() {
+        Collection = siesta.collection('Collection');
+        PersonModel = Collection.model('Person', {
+          id: 'id',
+          attributes: ['name', 'age']
+        });
+        CarModel = Collection.model('Car', {
+          id: 'id',
+          attributes: ['colour']
+        });
+        OtherCollection = siesta.collection('OtherCollection');
+        OtherModel = OtherCollection.model('Other', {
+          attributes: ['attr']
+        });
+        done();
+      });
+    });
+
+    it('map single collection & model', function(done) {
+      siesta.graph({
+        Collection: {
+          Car: {colour: 'red', id: 5}
+        }
+      }).then(function(results) {
+        var carResult = results.Collection.Car;
+        assert.equal(carResult.colour, 'red');
+        done();
+      }).catch(done);
+    });
+
+    it('map array', function(done) {
+      siesta.graph({
+        Collection: {
+          Car: [{colour: 'red', id: 5}, {colour: 'blue', id: 6}]
+        }
+      }).then(function(results) {
+        var carResults = results.Collection.Car;
+        assert.equal(carResults.length, 2);
+        done();
+      }).catch(done);
+    });
+
+    it('map multiple models', function(done) {
+      siesta.graph({
+        Collection: {
+          Car: [{colour: 'red', id: 5}, {colour: 'blue', id: 6}],
+          Person: {name: 'Mike', age: 24}
+        }
+      }).then(function(results) {
+        var carResults = results.Collection.Car,
+            personResults = results.Collection.Person;
+        assert.equal(carResults.length, 2);
+        assert.equal(personResults.name, 'Mike');
+        done();
+      }).catch(done);
+    });
+
+    it('map multiple collections', function(done) {
+      siesta.graph({
+        Collection: {
+          Car: [{colour: 'red', id: 5}, {colour: 'blue', id: 6}],
+          Person: {name: 'Mike', age: 24}
+        },
+        OtherCollection: {
+          Other: {attr: 1}
+        }
+      }).then(function(results) {
+        var carResults = results.Collection.Car,
+            personResults = results.Collection.Person,
+            otherResults = results.OtherCollection.Other;
+        assert.equal(carResults.length, 2);
+        assert.equal(personResults.name, 'Mike');
+        assert.equal(otherResults.attr, 1);
+        done();
+      }).catch(done);
+    });
+
+    it('invalid model', function(done) {
+      siesta.graph({
+        Collection: {
+          Car: [{colour: 'red', id: 5}, {colour: 'blue', id: 6}],
+          Invalid: {name: 'Mike', age: 24}
+        }
+      }).then(function() {
+        done('Should not have succeeeded');
+      }).catch(function(err) {
+        assert.equal(err.invalidModelName, 'Invalid');
+        assert.ok(err);
+        done();
+      });
+    });
+
+    it('invalid collection', function(done) {
+      siesta.graph({
+        Invalid: {
+          Car: [{colour: 'red', id: 5}, {colour: 'blue', id: 6}],
+          Person: {name: 'Mike', age: 24}
+        }
+      }).then(function() {
+        done('Should not have succeeeded');
+      }).catch(function(err) {
+        assert.equal(err.invalidCollectionName, 'Invalid');
+        assert.ok(err);
+        done();
+      });
+    });
+  });
+
 });
