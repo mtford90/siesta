451192e9f6c06055c316db6d2c155ec8022f3b36
Michael Ford <mtford@gmail.com>
2015-04-01 16:04:18 +0100

    detach underscore

--- core/log.js
+++ core/log.js
@@ -1,22 +1,22 @@
-(function () {
-    /**
-     * Dead simple logging service based on visionmedia/debug
-     * @module log
-     */
+(function() {
+  /**
+   * Dead simple logging service based on visionmedia/debug
+   * @module log
+   */
 
-    var debug = require('debug'),
-        argsarray = require('argsarray');
+  var debug = require('debug'),
+    argsarray = require('argsarray');
 
-    module.exports = function (name) {
-        var log = debug('siesta:' + name);
-        var fn = argsarray(function (args) {
-            log.call(log, args);
-        });
-        Object.defineProperty(fn, 'enabled', {
-            get: function () {
-                return debug.enabled(name);
-            }
-        });
-        return fn;
-    };
+  module.exports = function(name) {
+    var log = debug('siesta:' + name);
+    var fn = argsarray(function(args) {
+      log.call(log, args);
+    });
+    Object.defineProperty(fn, 'enabled', {
+      get: function() {
+        return debug.enabled(name);
+      }
+    });
+    return fn;
+  };
 })();
