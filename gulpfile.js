c331609e808d015e7d78519856c9f41a58ecae73
Michael Ford <mtford@gmail.com>
2015-04-02 04:43:22 +0100

    setup testing with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,31 @@ var gulp = require('gulp'),
   async = require('async'),
   plugins = require('gulp-load-plugins')();
 
-
 gulp.task('build', function() {
   return gulp.src(['./core/index.js'])
-    .pipe(plugins.webpack({}))
+    .pipe(plugins.webpack({
+      devtool: 'inline-source-map'
+    }))
     .pipe(plugins.rename('siesta.js'))
+    .pipe(gulp.dest('./build'))
+    .pipe(plugins.livereload({port: 47835}));
+});
+
+gulp.task('build-source-map', function() {
+  return gulp.src(['./core/index.js'])
+    .pipe(plugins.webpack({
+      devtool: 'inline-source-map'
+    }))
+    .pipe(plugins.rename('siesta-with-src-map.js'))
+    .pipe(gulp.dest('./build'))
+    .pipe(plugins.livereload({port: 47835}));
+});
+
+
+gulp.task('test-bundle', function() {
+  return gulp.src(['./test/**/*.spec.js'])
+    .pipe(plugins.webpack({}))
+    .pipe(plugins.rename('test-bundle.js'))
     .pipe(gulp.dest('./build'))
     .pipe(plugins.livereload({port: 47835}));
 });
@@ -22,29 +42,36 @@ gulp.task('dist', ['build'], function() {
     .pipe(gulp.dest('./build'));
 });
 
-gulp.task('release', ['dist'], function () {
+gulp.task('release', ['dist'], function() {
   return gulp.src(['./build/siesta.js', './build/siesta.min.js'])
     .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('serve', function() {
   plugins.connect.server({
-    root: './',
+    root: './test',
     port: 4001
   });
 });
 
-gulp.task('livereload:listen', function () {
+gulp.task('livereload:listen', function() {
   plugins.livereload.listen({port: 47835});
 });
 
 
 gulp.task('watch:js', function() {
-  return gulp.watch(['./core/**/*.js', './storage/**/*.js'], ['build']);
+  return gulp.watch(['./core/**/*.js', './storage/**/*.js'], ['build-source-map']);
 });
 
 gulp.task('watch:test', function() {
-  return gulp.watch(['./core/**/*.js', './storage/**/*.js'], ['build']);
+  return gulp.watch(['./test/**/*.js'], ['test-bundle']);
 });
 
-gulp.task('watch', ['watch:js', 'watch:test', 'livereload:listen', 'serve']);
+gulp.task('watch', [
+  'build-source-map',
+  'test-bundle',
+  'watch:js',
+  'watch:test',
+  'livereload:listen',
+  'serve'
+]);
