1f2b40df7eb7c798a64878331608cbefbe40c154
Michael Ford <mtford@gmail.com>
2015-04-09 06:56:15 +0100

    encapsulate cache objects

--- core/cache.js
+++ core/cache.js
@@ -3,288 +3,287 @@
  * Lookups are performed against the cache when mapping.
  * @module cache
  */
-(function() {
-
-  var log = require('./log')('cache'),
-    InternalSiestaError = require('./error').InternalSiestaError,
-    util = require('./util');
+var log = require('./log')('cache'),
+  InternalSiestaError = require('./error').InternalSiestaError,
+  util = require('./util');
 
 
-  var localCacheById = {},
-    localCache = {},
-    remoteCache = {};
+function Cache() {
+  this.reset();
+}
 
-  /**
-   * Clear out the cache.
-   */
-  function reset() {
-    remoteCache = {};
-    localCacheById = {};
-    localCache = {};
+Cache.prototype = {
+  reset: function() {
+    this.remote = {};
+    this.localById = {};
+    this.local = {};
   }
+};
 
-  /**
-   * Return the object in the cache given a local id (_id)
-   * @param  {String|Array} localId
-   * @return {ModelInstance}
-   */
-  function getViaLocalId(localId) {
-    if (util.isArray(localId)) return localId.map(function(x) {return localCacheById[x]});
-    else return localCacheById[localId];
-  }
 
-  /**
-   * Return the singleton object given a singleton model.
-   * @param  {Model} model
-   * @return {ModelInstance}
-   */
-  function getSingleton(model) {
-    var modelName = model.name;
-    var collectionName = model.collectionName;
-    var collectionCache = localCache[collectionName];
-    if (collectionCache) {
-      var typeCache = collectionCache[modelName];
-      if (typeCache) {
-        var objs = [];
-        for (var prop in typeCache) {
-          if (typeCache.hasOwnProperty(prop)) {
-            objs.push(typeCache[prop]);
-          }
-        }
-        if (objs.length > 1) {
-          var errStr = 'A singleton model has more than 1 object in the cache! This is a serious error. ' +
-            'Either a model has been modified after objects have already been created, or something has gone' +
-            'very wrong. Please file a bug report if the latter.';
-          throw new InternalSiestaError(errStr);
-        } else if (objs.length) {
-          return objs[0];
+var cache = new Cache();
+
+/**
+ * Return the object in the cache given a local id (_id)
+ * @param  {String|Array} localId
+ * @return {ModelInstance}
+ */
+function getViaLocalId(localId) {
+  if (util.isArray(localId)) return localId.map(function(x) {return cache.localById[x]});
+  else return cache.localById[localId];
+}
+
+/**
+ * Return the singleton object given a singleton model.
+ * @param  {Model} model
+ * @return {ModelInstance}
+ */
+function getSingleton(model) {
+  var modelName = model.name;
+  var collectionName = model.collectionName;
+  var collectionCache = cache.local[collectionName];
+  if (collectionCache) {
+    var typeCache = collectionCache[modelName];
+    if (typeCache) {
+      var objs = [];
+      for (var prop in typeCache) {
+        if (typeCache.hasOwnProperty(prop)) {
+          objs.push(typeCache[prop]);
         }
       }
+      if (objs.length > 1) {
+        var errStr = 'A singleton model has more than 1 object in the cache! This is a serious error. ' +
+          'Either a model has been modified after objects have already been created, or something has gone' +
+          'very wrong. Please file a bug report if the latter.';
+        throw new InternalSiestaError(errStr);
+      } else if (objs.length) {
+        return objs[0];
+      }
     }
-    return null;
   }
+  return null;
+}
 
-  /**
-   * Given a remote identifier and an options object that describes mapping/collection,
-   * return the model if cached.
-   * @param  {String|Array} remoteId
-   * @param  {Object} opts
-   * @param  {Object} opts.model
-   * @return {ModelInstance}
-   */
-  function getViaRemoteId(remoteId, opts) {
-    var cache = (remoteCache[opts.model.collectionName] || {})[opts.model.name] || {};
-    return util.isArray(remoteId) ? remoteId.map(function(x) {return cache[x]}) : cache[remoteId];
-  }
+/**
+ * Given a remote identifier and an options object that describes mapping/collection,
+ * return the model if cached.
+ * @param  {String|Array} remoteId
+ * @param  {Object} opts
+ * @param  {Object} opts.model
+ * @return {ModelInstance}
+ */
+function getViaRemoteId(remoteId, opts) {
+  var c = (cache.remote[opts.model.collectionName] || {})[opts.model.name] || {};
+  return util.isArray(remoteId) ? remoteId.map(function(x) {return c[x]}) : c[remoteId];
+}
 
-  /**
-   * Insert an object into the cache using a remote identifier defined by the mapping.
-   * @param  {ModelInstance} obj
-   * @param  {String} remoteId
-   * @param  {String} previousRemoteId If remote id has been changed, this is the old remote identifier
-   */
-  function remoteInsert(obj, remoteId, previousRemoteId) {
-    if (obj) {
-      var collectionName = obj.model.collectionName;
-      if (collectionName) {
-        if (!remoteCache[collectionName]) {
-          remoteCache[collectionName] = {};
+/**
+ * Insert an object into the cache using a remote identifier defined by the mapping.
+ * @param  {ModelInstance} obj
+ * @param  {String} remoteId
+ * @param  {String} previousRemoteId If remote id has been changed, this is the old remote identifier
+ */
+function remoteInsert(obj, remoteId, previousRemoteId) {
+  if (obj) {
+    var collectionName = obj.model.collectionName;
+    if (collectionName) {
+      if (!cache.remote[collectionName]) {
+        cache.remote[collectionName] = {};
+      }
+      var type = obj.model.name;
+      if (type) {
+        if (!cache.remote[collectionName][type]) {
+          cache.remote[collectionName][type] = {};
         }
-        var type = obj.model.name;
-        if (type) {
-          if (!remoteCache[collectionName][type]) {
-            remoteCache[collectionName][type] = {};
-          }
-          if (previousRemoteId) {
-            remoteCache[collectionName][type][previousRemoteId] = null;
-          }
-          var cachedObject = remoteCache[collectionName][type][remoteId];
-          if (!cachedObject) {
-            remoteCache[collectionName][type][remoteId] = obj;
-            log('Remote cache insert: ' + obj._dump(true));
-          } else {
-            // Something has gone really wrong. Only one object for a particular collection/type/remoteid combo
-            // should ever exist.
-            if (obj != cachedObject) {
-              var message = 'Object ' + collectionName.toString() + ':' + type.toString() + '[' + obj.model.id + '="' + remoteId + '"] already exists in the cache.' +
-                ' This is a serious error, please file a bug report if you are experiencing this out in the wild';
-              log(message, {
-                obj: obj,
-                cachedObject: cachedObject
-              });
-              throw new InternalSiestaError(message);
-            } else {
-              log('Object has already been inserted: ' + obj._dump(true));
-            }
-
-          }
+        if (previousRemoteId) {
+          cache.remote[collectionName][type][previousRemoteId] = null;
+        }
+        var cachedObject = cache.remote[collectionName][type][remoteId];
+        if (!cachedObject) {
+          cache.remote[collectionName][type][remoteId] = obj;
+          log('Remote cache insert: ' + obj._dump(true));
         } else {
-          throw new InternalSiestaError('Model has no type', {
-            model: obj.model,
-            obj: obj
-          });
+          // Something has gone really wrong. Only one object for a particular collection/type/remoteid combo
+          // should ever exist.
+          if (obj != cachedObject) {
+            var message = 'Object ' + collectionName.toString() + ':' + type.toString() + '[' + obj.model.id + '="' + remoteId + '"] already exists in the cache.' +
+              ' This is a serious error, please file a bug report if you are experiencing this out in the wild';
+            log(message, {
+              obj: obj,
+              cachedObject: cachedObject
+            });
+            throw new InternalSiestaError(message);
+          } else {
+            log('Object has already been inserted: ' + obj._dump(true));
+          }
+
         }
       } else {
-        throw new InternalSiestaError('Model has no collection', {
+        throw new InternalSiestaError('Model has no type', {
           model: obj.model,
           obj: obj
         });
       }
     } else {
-      var msg = 'Must pass an object when inserting to cache';
-      log(msg);
-      throw new InternalSiestaError(msg);
-    }
-  }
-
-
-  function _remoteCache() {
-    return remoteCache
-  }
-
-  function _localCache() {
-    return localCacheById;
-  }
-
-  /**
-   * Query the cache
-   * @param  {Object} opts Object describing the query
-   * @return {ModelInstance}
-   * @example
-   * ```js
-   * cache.get({_id: '5'}); // Query by local id
-   * cache.get({remoteId: '5', mapping: myMapping}); // Query by remote id
-   * ```
-   */
-  function get(opts) {
-    log('get', opts);
-    var obj, idField, remoteId;
-    var localId = opts.localId;
-    if (localId) {
-      obj = getViaLocalId(localId);
-      if (obj) {
-        return obj;
-      } else {
-        if (opts.model) {
-          idField = opts.model.id;
-          remoteId = opts[idField];
-          log(idField + '=' + remoteId);
-          return getViaRemoteId(remoteId, opts);
-        } else {
-          return null;
-        }
-      }
-    } else if (opts.model) {
-      idField = opts.model.id;
-      remoteId = opts[idField];
-      if (remoteId) {
-        return getViaRemoteId(remoteId, opts);
-      } else if (opts.model.singleton) {
-        return getSingleton(opts.model);
-      }
-    } else {
-      log('Invalid opts to cache', {
-        opts: opts
+      throw new InternalSiestaError('Model has no collection', {
+        model: obj.model,
+        obj: obj
       });
     }
-    return null;
+  } else {
+    var msg = 'Must pass an object when inserting to cache';
+    log(msg);
+    throw new InternalSiestaError(msg);
   }
+}
 
-  /**
-   * Insert an object into the cache.
-   * @param  {ModelInstance} obj
-   * @throws {InternalSiestaError} An object with _id/remoteId already exists. Not thrown if same obhect.
-   */
-  function insert(obj) {
-    var localId = obj.localId;
-    if (localId) {
-      var collectionName = obj.model.collectionName;
-      var modelName = obj.model.name;
-      if (!localCacheById[localId]) {
-        localCacheById[localId] = obj;
-        if (!localCache[collectionName]) localCache[collectionName] = {};
-        if (!localCache[collectionName][modelName]) localCache[collectionName][modelName] = {};
-        localCache[collectionName][modelName][localId] = obj;
-      } else {
-        // Something has gone badly wrong here. Two objects should never exist with the same _id
-        if (localCacheById[localId] != obj) {
-          var message = 'Object with localId="' + localId.toString() + '" is already in the cache. ' +
-            'This is a serious error. Please file a bug report if you are experiencing this out in the wild';
-          log(message);
-          throw new InternalSiestaError(message);
-        }
-      }
-    }
-    var idField = obj.idField;
-    var remoteId = obj[idField];
-    if (remoteId) {
-      remoteInsert(obj, remoteId);
+
+function _remoteCache() {
+  return cache.remote
+}
+
+function _localCache() {
+  return cache.localById;
+}
+
+/**
+ * Query the cache
+ * @param  {Object} opts Object describing the query
+ * @return {ModelInstance}
+ * @example
+ * ```js
+ * cache.get({_id: '5'}); // Query by local id
+ * cache.get({remoteId: '5', mapping: myMapping}); // Query by remote id
+ * ```
+ */
+function get(opts) {
+  log('get', opts);
+  var obj, idField, remoteId;
+  var localId = opts.localId;
+  if (localId) {
+    obj = getViaLocalId(localId);
+    if (obj) {
+      return obj;
     } else {
-      log('No remote id ("' + idField + '") so wont be placing in the remote cache', obj);
-    }
-  }
-
-  /**
-   * Returns true if object is in the cache
-   * @param  {ModelInstance} obj
-   * @return {boolean}
-   */
-  function contains(obj) {
-    var q = {
-      localId: obj.localId
-    };
-    var model = obj.model;
-    if (model.id) {
-      if (obj[model.id]) {
-        q.model = model;
-        q[model.id] = obj[model.id];
+      if (opts.model) {
+        idField = opts.model.id;
+        remoteId = opts[idField];
+        log(idField + '=' + remoteId);
+        return getViaRemoteId(remoteId, opts);
+      } else {
+        return null;
       }
     }
-    return !!get(q);
+  } else if (opts.model) {
+    idField = opts.model.id;
+    remoteId = opts[idField];
+    if (remoteId) {
+      return getViaRemoteId(remoteId, opts);
+    } else if (opts.model.singleton) {
+      return getSingleton(opts.model);
+    }
+  } else {
+    log('Invalid opts to cache', {
+      opts: opts
+    });
   }
+  return null;
+}
 
-  /**
-   * Removes the object from the cache (if it's actually in the cache) otherwises throws an error.
-   * @param  {ModelInstance} obj
-   * @throws {InternalSiestaError} If object already in the cache.
-   */
-  function remove(obj) {
-    if (contains(obj)) {
-      var collectionName = obj.model.collectionName;
-      var modelName = obj.model.name;
-      var localId = obj.localId;
-      if (!modelName) throw InternalSiestaError('No mapping name');
-      if (!collectionName) throw InternalSiestaError('No collection name');
-      if (!localId) throw InternalSiestaError('No localId');
-      delete localCache[collectionName][modelName][localId];
-      delete localCacheById[localId];
-      if (obj.model.id) {
-        var remoteId = obj[obj.model.id];
-        if (remoteId) {
-          delete remoteCache[collectionName][modelName][remoteId];
-        }
+/**
+ * Insert an object into the cache.
+ * @param  {ModelInstance} obj
+ * @throws {InternalSiestaError} An object with _id/remoteId already exists. Not thrown if same obhect.
+ */
+function insert(obj) {
+  var localId = obj.localId;
+  if (localId) {
+    var collectionName = obj.model.collectionName;
+    var modelName = obj.model.name;
+    if (!cache.localById[localId]) {
+      cache.localById[localId] = obj;
+      if (!cache.local[collectionName]) cache.local[collectionName] = {};
+      if (!cache.local[collectionName][modelName]) cache.local[collectionName][modelName] = {};
+      cache.local[collectionName][modelName][localId] = obj;
+    } else {
+      // Something has gone badly wrong here. Two objects should never exist with the same _id
+      if (cache.localById[localId] != obj) {
+        var message = 'Object with localId="' + localId.toString() + '" is already in the cache. ' +
+          'This is a serious error. Please file a bug report if you are experiencing this out in the wild';
+        log(message);
+        throw new InternalSiestaError(message);
       }
     }
   }
-
-
-  exports._remoteCache = _remoteCache;
-  exports._localCache = _localCache;
-  Object.defineProperty(exports, '_localCacheByType', {
-    get: function() {
-      return localCache;
-    }
-  });
-  exports.get = get;
-  exports.insert = insert;
-  exports.remoteInsert = remoteInsert;
-  exports.reset = reset;
-  exports.contains = contains;
-  exports.remove = remove;
-  exports.getSingleton = getSingleton;
-  exports.getViaLocalId = getViaLocalId;
-  exports.getViaRemoteId = getViaRemoteId;
-  exports.count = function() {
-    return Object.keys(localCacheById).length;
+  var idField = obj.idField;
+  var remoteId = obj[idField];
+  if (remoteId) {
+    remoteInsert(obj, remoteId);
+  } else {
+    log('No remote id ("' + idField + '") so wont be placing in the remote cache', obj);
   }
-})();
+}
+
+/**
+ * Returns true if object is in the cache
+ * @param  {ModelInstance} obj
+ * @return {boolean}
+ */
+function contains(obj) {
+  var q = {
+    localId: obj.localId
+  };
+  var model = obj.model;
+  if (model.id) {
+    if (obj[model.id]) {
+      q.model = model;
+      q[model.id] = obj[model.id];
+    }
+  }
+  return !!get(q);
+}
+
+/**
+ * Removes the object from the cache (if it's actually in the cache) otherwises throws an error.
+ * @param  {ModelInstance} obj
+ * @throws {InternalSiestaError} If object already in the cache.
+ */
+function remove(obj) {
+  if (contains(obj)) {
+    var collectionName = obj.model.collectionName;
+    var modelName = obj.model.name;
+    var localId = obj.localId;
+    if (!modelName) throw InternalSiestaError('No mapping name');
+    if (!collectionName) throw InternalSiestaError('No collection name');
+    if (!localId) throw InternalSiestaError('No localId');
+    delete cache.local[collectionName][modelName][localId];
+    delete cache.localById[localId];
+    if (obj.model.id) {
+      var remoteId = obj[obj.model.id];
+      if (remoteId) {
+        delete cache.remote[collectionName][modelName][remoteId];
+      }
+    }
+  }
+}
+
+
+exports._remoteCache = _remoteCache;
+exports._localCache = _localCache;
+Object.defineProperty(exports, '_localCacheByType', {
+  get: function() {
+    return cache.local;
+  }
+});
+exports.get = get;
+exports.insert = insert;
+exports.remoteInsert = remoteInsert;
+exports.reset = cache.reset.bind(cache);
+exports.contains = contains;
+exports.remove = remove;
+exports.getSingleton = getSingleton;
+exports.getViaLocalId = getViaLocalId;
+exports.getViaRemoteId = getViaRemoteId;
+exports.count = function() {
+  return Object.keys(cache.localById).length;
+};
