36e7c899875322cb14b6ba932fbea8f57e0aac0b
Michael Ford <mtford@gmail.com>
2015-04-10 08:44:38 +0100

    fix same bug with ManyToMany

--- test/remove.spec.js
+++ test/remove.spec.js
@@ -10,6 +10,64 @@ describe('remove models from object graph', function() {
     siesta.ext.storageEnabled = false;
   });
 
+  describe('one to one', function() {
+    var MyCollection, Car, Person;
+    var car, person;
+
+    beforeEach(function(done) {
+      siesta.reset(function() {
+        MyCollection = siesta.collection('MyCollection');
+        Car = MyCollection.model('Car', {
+          id: 'id',
+          attributes: ['colour'],
+          relationships: {
+            owner: {
+              model: 'Person',
+              type: 'OneToOne',
+              reverse: 'car'
+            }
+          }
+        });
+        Person = MyCollection.model('Person', {
+          id: 'id',
+          attributes: ['name']
+        });
+        siesta.install(function() {
+          Car.graph({
+            id: 1,
+            colour: 'red',
+            owner: {
+              id: 2,
+              name: 'mike'
+            }
+          }).then(function(res) {
+            car = res;
+            person = res.owner;
+            done();
+          })
+        });
+      });
+    });
+
+    it('removal', function(done) {
+      person
+        .remove()
+        .then(function() {
+          assert.notOk(car.owner);
+          done();
+        }).catch(done);
+    });
+
+    it('reverse removal', function(done) {
+      car
+        .remove()
+        .then(function() {
+          assert.notOk(person.car);
+          done();
+        }).catch(done);
+    });
+  });
+
   describe('one to many', function() {
     var MyCollection, Car, Person;
     var car, person;
@@ -54,6 +112,64 @@ describe('remove models from object graph', function() {
         .remove()
         .then(function() {
           assert.notOk(car.owner);
+          done();
+        }).catch(done);
+    });
+
+    it('reverse removal', function(done) {
+      car
+        .remove()
+        .then(function() {
+          assert.notOk(person.cars.length);
+          done();
+        }).catch(done);
+    });
+  });
+
+  describe('many to many', function() {
+    var MyCollection, Car, Person;
+    var car, person;
+
+    beforeEach(function(done) {
+      siesta.reset(function() {
+        MyCollection = siesta.collection('MyCollection');
+        Car = MyCollection.model('Car', {
+          id: 'id',
+          attributes: ['colour'],
+          relationships: {
+            owners: {
+              model: 'Person',
+              type: 'ManyToMany',
+              reverse: 'cars'
+            }
+          }
+        });
+        Person = MyCollection.model('Person', {
+          id: 'id',
+          attributes: ['name']
+        });
+        siesta.install(function() {
+          Car.graph({
+            id: 1,
+            colour: 'red',
+            owners: [{
+              id: 2,
+              name: 'mike'
+            }]
+          }).then(function(res) {
+            car = res;
+            person = res.owners[0];
+            done();
+          })
+        });
+      });
+    });
+
+    it('removal', function(done) {
+      person
+        .remove()
+        .then(function() {
+          assert.notOk(car.owners.length);
           done();
         }).catch(done);
     });
