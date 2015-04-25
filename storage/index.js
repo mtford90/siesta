d433df47b055d2d17f57d6d78874dc1d8ae31855
Michael Ford <mtford@gmail.com>
2015-04-25 22:17:45 +0100

    abstracted storage into an object

--- storage/index.js
+++ storage/index.js
@@ -19,6 +19,29 @@ function Storage(name) {
   this.unsavedObjectsHash = {};
   this.unsavedObjectsByCollection = {};
 
+  Object.defineProperties(this, {
+    _unsavedObjects: {
+      get: function() {
+        return this.unsavedObjects
+      }
+    },
+    _unsavedObjectsHash: {
+      get: function() {
+        return this.unsavedObjectsHash
+      }
+    },
+    _unsavedObjectsByCollection: {
+      get: function() {
+        return this.unsavedObjectsByCollection
+      }
+    },
+    _pouch: {
+      get: function() {
+        return this.pouch
+      }
+    }
+  });
+
   this.pouch = new PouchDB(name, {auto_compaction: true})
 }
 
@@ -39,7 +62,7 @@ Storage.prototype = {
   },
   saveToPouch: function(objects, cb) {
     var conflicts = [];
-    var serialisedDocs = objects.map(_serialise);
+    var serialisedDocs = objects.map(this._serialise.bind(this));
     this.pouch.bulkDocs(serialisedDocs).then(function(resp) {
       for (var i = 0; i < resp.length; i++) {
         var response = resp[i];
@@ -77,76 +100,13 @@ Storage.prototype = {
       .catch(function(err) {
         cb(err);
       })
-  }
-};
-
-
-var storage = new Storage();
-
-
-function _initMeta() {
-  return {dateFields: []};
-}
-
-function fullyQualifiedModelName(collectionName, modelName) {
-  return collectionName + '.' + modelName;
-}
-
-
-if (typeof PouchDB == 'undefined') {
-  siesta.ext.storageEnabled = false;
-  console.log('PouchDB is not present therefore storage is disabled.');
-}
-else {
-
-  /**
-   * Sometimes siesta needs to store some extra information about the model instance.
-   * @param serialised
-   * @private
-   */
-  function _addMeta(serialised) {
-    serialised.siesta_meta = _initMeta();
-    for (var prop in serialised) {
-      if (serialised.hasOwnProperty(prop)) {
-        if (serialised[prop] instanceof Date) {
-          serialised.siesta_meta.dateFields.push(prop);
-          serialised[prop] = serialised[prop].getTime();
-        }
-      }
-    }
-  }
-
-  function _processMeta(datum) {
-    var meta = datum.siesta_meta || _initMeta();
-    meta.dateFields.forEach(function(dateField) {
-      var value = datum[dateField];
-      if (!(value instanceof Date)) {
-        datum[dateField] = new Date(value);
-      }
-    });
-    delete datum.siesta_meta;
-  }
-
-  function constructIndexDesignDoc(collectionName, modelName) {
-    var fullyQualifiedName = fullyQualifiedModelName(collectionName, modelName);
-    var views = {};
-    views[fullyQualifiedName] = {
-      map: function(doc) {
-        if (doc.collection == '$1' && doc.model == '$2') emit(doc.collection + '.' + doc.model, doc);
-      }.toString().replace('$1', collectionName).replace('$2', modelName)
-    };
-    return {
-      _id: '_design/' + fullyQualifiedName,
-      views: views
-    };
-  }
-
+  },
   /**
    * Ensure that the PouchDB index for the given model exists, creating it if not.
    * @param model
    * @param cb
    */
-  function ensureIndexInstalled(model, cb) {
+  ensureIndexInstalled: function(model, cb) {
     function fn(resp) {
       var err;
       if (!resp.ok) {
@@ -158,45 +118,78 @@ else {
       cb(err);
     }
 
-    storage.pouch
-      .put(constructIndexDesignDoc(model.collectionName, model.name))
+    this
+      .pouch
+      .put(this.constructIndexDesignDoc(model.collectionName, model.name))
       .then(fn)
       .catch(fn);
-  }
-
+  },
   /**
-   * Serialise a model into a format that PouchDB bulkDocs API can process
-   * @param {ModelInstance} modelInstance
+   *
+   * @param opts
+   * @param [opts.collectionName]
+   * @param [opts.modelName]
+   * @param [opts.model]
+   * @param cb
+   * @private
    */
-  function _serialise(modelInstance) {
-    var serialised = {};
-    var __values = modelInstance.__values;
-    serialised = util.extend(serialised, __values);
-    Object.keys(serialised).forEach(function(k) {
-      serialised[k.replace(UNDERSCORE, '@')] = __values[k];
-    });
-    _addMeta(serialised);
-    serialised['collection'] = modelInstance.collectionName;
-    serialised['model'] = modelInstance.modelName;
-    serialised['_id'] = modelInstance.localId;
-    if (modelInstance.removed) serialised['_deleted'] = true;
-    var rev = modelInstance._rev;
-    if (rev) serialised['_rev'] = rev;
-    serialised = modelInstance._relationshipNames.reduce(function(memo, n) {
-      var val = modelInstance[n];
-      if (siesta.isArray(val)) {
-        memo[n] = util.pluck(val, 'localId');
-      }
-      else if (val) {
-        memo[n] = val.localId;
-      }
-      return memo;
-    }, serialised);
-    return serialised;
-  }
+  loadModel: function(opts, cb) {
+    var loaded = {};
+    var collectionName = opts.collectionName,
+      modelName = opts.modelName,
+      model = opts.model;
+    if (model) {
+      collectionName = model.collectionName;
+      modelName = model.name;
+    }
 
-  function _prepareDatum(rawDatum, model) {
-    _processMeta(rawDatum);
+    var fullyQualifiedName = this.fullyQualifiedModelName(collectionName, modelName);
+    var Model = siesta.app.collectionRegistry[collectionName][modelName];
+    this
+      .pouch
+      .query(fullyQualifiedName)
+      .then(function(resp) {
+        console.log('Queried pouch successfully');
+        var rows = resp.rows;
+        var data = util.pluck(rows, 'value').map(function(datum) {
+          return this._prepareDatum(datum, Model);
+        }.bind(this));
+
+        data.map(function(datum) {
+          var remoteId = datum[Model.id];
+          if (remoteId) {
+            if (loaded[remoteId]) {
+              console.error('Duplicates detected in storage. You have encountered a serious bug. Please report this.');
+            }
+            else {
+              loaded[remoteId] = datum;
+            }
+          }
+        });
+
+        Model._graph(data, {
+          _ignoreInstalled: true,
+          disableevents: true,
+          fromStorage: true
+        }, function(err, instances) {
+          if (!err) {
+            console.log('Loaded ' + instances ? instances.length.toString() : 0 + ' instances for ' + fullyQualifiedName);
+            this._listener = this.listener.bind(this);
+            model.on('*', this._listener);
+          }
+          else {
+            console.error('Error loading models', err);
+          }
+          cb(err, instances);
+        }.bind(this));
+      }.bind(this))
+      .catch(function(err) {
+        cb(err);
+      });
+
+  },
+  _prepareDatum: function(rawDatum, model) {
+    this._processMeta(rawDatum);
     delete rawDatum.collection;
     delete rawDatum.model;
     rawDatum.localId = rawDatum._id;
@@ -222,152 +215,136 @@ else {
 
     });
     return datum;
-  }
+  },
+  _processMeta: function(datum) {
+    var meta = datum.siesta_meta || this._initMeta();
+    meta.dateFields.forEach(function(dateField) {
+      var value = datum[dateField];
+      if (!(value instanceof Date)) {
+        datum[dateField] = new Date(value);
+      }
+    });
+    delete datum.siesta_meta;
+  },
+
+  _initMeta: function() {
+    return {dateFields: []};
+  },
 
   /**
-   *
-   * @param opts
-   * @param [opts.collectionName]
-   * @param [opts.modelName]
-   * @param [opts.model]
-   * @param cb
-   * @private
+   * Sometimes siesta needs to store some extra information about the model instance.
+   * @param serialised
    */
-  function loadModel(opts, cb) {
-    var loaded = {};
-    var collectionName = opts.collectionName,
-      modelName = opts.modelName,
-      model = opts.model;
-    if (model) {
-      collectionName = model.collectionName;
-      modelName = model.name;
+  _addMeta: function(serialised) {
+    serialised.siesta_meta = this._initMeta();
+    for (var prop in serialised) {
+      if (serialised.hasOwnProperty(prop)) {
+        if (serialised[prop] instanceof Date) {
+          serialised.siesta_meta.dateFields.push(prop);
+          serialised[prop] = serialised[prop].getTime();
+        }
+      }
     }
+  },
 
-    var fullyQualifiedName = fullyQualifiedModelName(collectionName, modelName);
-    var Model = siesta.app.collectionRegistry[collectionName][modelName];
-    storage.pouch
-      .query(fullyQualifiedName)
-      .then(function(resp) {
-        console.log('Queried pouch successfully');
-        var rows = resp.rows;
-        var data = util.pluck(rows, 'value').map(function(datum) {
-          return _prepareDatum(datum, Model);
-        });
-
-        data.map(function(datum) {
-          var remoteId = datum[Model.id];
-          if (remoteId) {
-            if (loaded[remoteId]) {
-              console.error('Duplicates detected in storage. You have encountered a serious bug. Please report this.');
-            }
-            else {
-              loaded[remoteId] = datum;
-            }
-          }
-        });
-
-        Model._graph(data, {
-          _ignoreInstalled: true,
-          disableevents: true,
-          fromStorage: true
-        }, function(err, instances) {
-          if (!err) {
-            console.log('Loaded ' + instances ? instances.length.toString() : 0 + ' instances for ' + fullyQualifiedName);
-            model.on('*', listener);
-          }
-          else {
-            console.error('Error loading models', err);
-          }
-          cb(err, instances);
-        });
-      })
-      .catch(function(err) {
-        cb(err);
-      });
-
-  }
-
-
-  function listener(n) {
+  listener: function(n) {
     var changedObject = n.obj,
       ident = changedObject.localId;
     if (!changedObject) {
       throw new _i.error.InternalSiestaError('No obj field in notification received by storage extension');
     }
-    if (!(ident in storage.unsavedObjectsHash)) {
-      storage.unsavedObjectsHash[ident] = changedObject;
-      storage.unsavedObjects.push(changedObject);
+    if (!(ident in this.unsavedObjectsHash)) {
+      this.unsavedObjectsHash[ident] = changedObject;
+      this.unsavedObjects.push(changedObject);
       var collectionName = changedObject.collectionName;
-      if (!storage.unsavedObjectsByCollection[collectionName]) {
-        storage.unsavedObjectsByCollection[collectionName] = {};
+      if (!this.unsavedObjectsByCollection[collectionName]) {
+        this.unsavedObjectsByCollection[collectionName] = {};
       }
       var modelName = changedObject.model.name;
-      if (!storage.unsavedObjectsByCollection[collectionName][modelName]) {
-        storage.unsavedObjectsByCollection[collectionName][modelName] = {};
+      if (!this.unsavedObjectsByCollection[collectionName][modelName]) {
+        this.unsavedObjectsByCollection[collectionName][modelName] = {};
       }
-      storage.unsavedObjectsByCollection[collectionName][modelName][ident] = changedObject;
+      this.unsavedObjectsByCollection[collectionName][modelName][ident] = changedObject;
     }
+  },
+  _reset: function(cb) {
+    if (this._listener) siesta.removeListener('Siesta', this._listener);
+    this.unsavedObjects = [];
+    this.unsavedObjectsHash = {};
+
+    this
+      .pouch
+      .allDocs()
+      .then(function(results) {
+        var docs = results.rows.map(function(r) {
+          return {_id: r.id, _rev: r.value.rev, _deleted: true};
+        });
+
+        this.pouch
+          .bulkDocs(docs)
+          .then(function() {cb()})
+          .catch(cb);
+      }.bind(this))
+      .catch(cb);
+  },
+  /**
+   * Serialise a model into a format that PouchDB bulkDocs API can process
+   * @param {ModelInstance} modelInstance
+   */
+  _serialise: function(modelInstance) {
+    console.log(1);
+    var serialised = {};
+    var __values = modelInstance.__values;
+    serialised = util.extend(serialised, __values);
+    Object.keys(serialised).forEach(function(k) {
+      serialised[k.replace(UNDERSCORE, '@')] = __values[k];
+    });
+    this._addMeta(serialised);
+    serialised['collection'] = modelInstance.collectionName;
+    serialised['model'] = modelInstance.modelName;
+    serialised['_id'] = modelInstance.localId;
+    if (modelInstance.removed) serialised['_deleted'] = true;
+    var rev = modelInstance._rev;
+    if (rev) serialised['_rev'] = rev;
+    serialised = modelInstance._relationshipNames.reduce(function(memo, n) {
+      var val = modelInstance[n];
+      if (siesta.isArray(val)) {
+        memo[n] = util.pluck(val, 'localId');
+      }
+      else if (val) {
+        memo[n] = val.localId;
+      }
+      return memo;
+    }, serialised);
+    return serialised;
+  },
+  constructIndexDesignDoc: function(collectionName, modelName) {
+    var fullyQualifiedName = this.fullyQualifiedModelName(collectionName, modelName);
+    var views = {};
+    views[fullyQualifiedName] = {
+      map: function(doc) {
+        if (doc.collection == '$1' && doc.model == '$2') emit(doc.collection + '.' + doc.model, doc);
+      }.toString().replace('$1', collectionName).replace('$2', modelName)
+    };
+    return {
+      _id: '_design/' + fullyQualifiedName,
+      views: views
+    };
+  },
+  fullyQualifiedModelName: function(collectionName, modelName) {
+    return collectionName + '.' + modelName;
   }
+};
 
-  util.extend(storage, {
-    loadModel: loadModel,
-    save: storage.save.bind(storage),
-    _serialise: _serialise,
-    ensureIndexInstalled: ensureIndexInstalled,
-    /**
-     * Used during testing only.
-     * @param cb
-     * @private
-     */
-    _reset: function(cb) {
-      siesta.removeListener('Siesta', listener);
-      storage.unsavedObjects = [];
-      storage.unsavedObjectsHash = {};
-      //storage.pouch
-      //  .destroy()
-      //  .then(function() {
-      //    initDb();
-      //  })
-      //  .catch(cb);
-      storage.pouch
-        .allDocs()
-        .then(function(results) {
-          var docs = results.rows.map(function(r) {
-            return {_id: r.id, _rev: r.value.rev, _deleted: true};
-          });
 
-          storage.pouch
-            .bulkDocs(docs)
-            .then(function() {cb()})
-            .catch(cb);
-        })
-        .catch(cb);
-    }
-  });
+var storage = new Storage();
 
-  Object.defineProperties(storage, {
-    _unsavedObjects: {
-      get: function() {
-        return storage.unsavedObjects
-      }
-    },
-    _unsavedObjectsHash: {
-      get: function() {
-        return storage.unsavedObjectsHash
-      }
-    },
-    _unsavedObjectsByCollection: {
-      get: function() {
-        return storage.unsavedObjectsByCollection
-      }
-    },
-    _pouch: {
-      get: function() {
-        return storage.pouch
-      }
-    }
-  });
 
+if (typeof PouchDB == 'undefined') {
+  siesta.ext.storageEnabled = false;
+  console.log('PouchDB is not present therefore storage is disabled.');
+}
+else {
   if (!siesta.ext) siesta.ext = {};
   siesta.ext.storage = storage;
 
