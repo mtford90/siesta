85f989666098a48e1720211a6e7c73d49477ab0a
Michael Ford <mtford@gmail.com>
2015-05-01 08:47:53 +0100

    added spec for cascading delete

---
+++ test/delete.spec.js
@@ -0,0 +1,10 @@
+describe('Models', function() {
+
+  var app = siesta.createApp('Models', {storage: false});
+  var assert = require('chai').assert;
+
+
+  describe('cascade', function() {
+
+  });
+});
